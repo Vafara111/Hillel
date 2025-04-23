3786c8bb010e7574d8e7d0b390dd9c734606a05e
Vafara111 <Vafara111@gmail.com>
2025-04-23 14:41:53 +0300

    Seven commit

---
+++ Homework 7,4.py
@@ -0,0 +1,6 @@
+def common_elements():
+    set3 = set(range(0, 100, 3))
+    set5 = set(range(0, 100, 5))
+    return set3.intersection(set5)
+
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
