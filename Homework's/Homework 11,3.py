a1bcbfa9338e55e7a7df6393760e6d9143b7588c
Vafara111 <Vafara111@gmail.com>
2025-05-20 15:58:08 +0300

    Eleven commit

--- Homework's/Homework 11,3.py
+++ Homework's/Homework 11,3.py
@@ -2,9 +2,8 @@ def is_even(number: int) -> bool:
     numbers = ("2","4","6","8")
     number = str(number)
 
-    for i in numbers:
-        if number[-1] == i:
-            return True
+    if number[-1] in numbers:
+        return True
     return False
 
 
